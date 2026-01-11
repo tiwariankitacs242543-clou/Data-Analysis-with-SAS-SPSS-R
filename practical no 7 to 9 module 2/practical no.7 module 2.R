9d1cb2ff3c869020ef125f6f2a628e14c52c1aee
Ankita Tiwari <tiwariankitacs242543@gmail.com>
2026-01-11 12:16:29 +0530

    Update practical no.7 module 2.R

--- practical no 7 to 9 module 2/practical no.7 module 2.R
+++ practical no 7 to 9 module 2/practical no.7 module 2.R
@@ -1,11 +1,27 @@
-data1 <- read.csv("C:/Users/Admin/Desktop/student data.csv")
+data1 <- read.csv("C:/Users/Admin/Desktop/ANKITA TIWARI (data science R)/datasets/student data.csv")
 
-colnames(data1)
+print(data1)
 
-colnames(data1) <- c("ID", "Age", "Study_Hours", "Attendance", "Score")
+colnames(data1)[colnames(data1) == "Score (%)"] <- "Score"
 
 data1$Age <- as.factor(data1$Age)
 
-anova_one <- aov(Score ~ Age, data = data1)
+group_means <- tapply(data1$Score, data1$Age, mean)
 
-summary(anova_one)
+grand_mean <- mean(data1$Score)
+
+group_sizes <- table(data1$Age)
+
+SSB <- sum(group_sizes * (group_means - grand_mean)^2)
+
+SSW <- sum((data1$Score - group_means[data1$Age])^2)
+
+df_between <- length(group_means) - 1
+df_within <- nrow(data1) - length(group_means)
+
+MSB <- SSB / df_between
+MSW <- SSW / df_within
+
+F_value <- MSB / MSW
+F_value
+
