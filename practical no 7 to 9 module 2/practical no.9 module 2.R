d4ae6c530e321ba0aa16469916c6baad48730e81
Ankita Tiwari <tiwariankitacs242543@gmail.com>
2026-01-11 12:20:07 +0530

    Update practical no.9 module 2.R

--- practical no 7 to 9 module 2/practical no.9 module 2.R
+++ practical no 7 to 9 module 2/practical no.9 module 2.R
@@ -1,12 +1,39 @@
-data3 <- read.csv("C:/Users/Admin/Desktop/Suicides_in_India_random_500 (1).csv")
+#Chi-Square Test of Independence
+data <- read.csv("C:/Users/Admin/Desktop/ANKITA TIWARI (data science R)/datasets/Suicides_in_India_random_500 (1).csv")
 
-head(data3)
+observed <- table(data$Gender, data$Age_group)
 
-data3$Gender <- as.factor(data3$Gender)
-data3$Age_group <- as.factor(data3$Age_group)
+row_totals <- rowSums(observed)
+col_totals <- colSums(observed)
+grand_total <- sum(observed)
 
-table_data <- table(data3$Gender, data3$Age_group)
+expected <- outer(row_totals, col_totals) / grand_total
 
-table_data
+chi_square <- sum((observed - expected)^2 / expected)
 
-chisq.test(table_data)
+df <- (nrow(observed) - 1) * (ncol(observed) - 1)
+
+observed
+expected
+chi_square
+df
+
+#Chi-Square Test of Goodness of Fit
+data <- read.csv("C:/Users/Admin/Desktop/ANKITA TIWARI (data science R)/datasets/Suicides_in_India_random_500 (1).csv")
+
+observed <- tapply(data$Total, data$Age_group, sum)
+
+k <- length(observed)
+
+expected <- rep(sum(observed) / k, k)
+
+chi_square <- sum((observed - expected)^2 / expected)
+
+df <- k - 1
+
+observed
+expected
+chi_square
+df
+
+
