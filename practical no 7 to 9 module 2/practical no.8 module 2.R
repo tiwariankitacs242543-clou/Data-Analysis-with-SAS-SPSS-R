8cfe0807c561a38c37250e8297b20bf31874ff3e
Ankita Tiwari <tiwariankitacs242543@gmail.com>
2026-01-11 12:18:07 +0530

    Update practical no.8 module 2.R

--- practical no 7 to 9 module 2/practical no.8 module 2.R
+++ practical no 7 to 9 module 2/practical no.8 module 2.R
@@ -1,13 +1,32 @@
-titanic <- read.csv("C:/Users/Admin/Desktop/ANKITA TIWARI (data science R)/datasets/titanic.csv")
+data2 <- read.csv("C:/Users/Admin/Desktop/ANKITA TIWARI (data science R)/datasets/titanic.csv")
 
-str(titanic)
+data2$Pclass <- as.factor(data2$Pclass)
+data2$Sex <- as.factor(data2$Sex)
 
-titanic$Sex <- as.factor(titanic$Sex)
-titanic$Pclass <- as.factor(titanic$Pclass)
+grand_mean <- mean(data2$Fare)
 
-titanic_clean <- na.omit(titanic[, c("Fare", "Sex", "Pclass")])
+tm_means <- tapply(data2$Fare, data2$Pclass, mean)
+tm_sizes <- table(data2$Pclass)
+SS_TM <- sum(tm_sizes * (tm_means - grand_mean)^2)
 
-anova_two <- aov(Fare ~ Sex * Pclass, data = titanic_clean)
+gender_means <- tapply(data2$Fare, data2$Sex, mean)
+gender_sizes <- table(data2$Sex)
+SS_G <- sum(gender_sizes * (gender_means - grand_mean)^2)
 
-summary(anova_two)
+SS_total <- sum((data2$Fare - grand_mean)^2)
+SS_error <- SS_total - SS_TM - SS_G
+
+df_TM <- length(tm_means) - 1
+df_G <- length(gender_means) - 1
+df_error <- nrow(data2) - length(tm_means) - length(gender_means) + 1
+
+MS_TM <- SS_TM / df_TM
+MS_G <- SS_G / df_G
+MS_error <- SS_error / df_error
+
+F_TM <- MS_TM / MS_error
+F_G <- MS_G / MS_error
+
+F_TM
+F_G
 
